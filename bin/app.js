0659de7290d84e4ac2fd31a097c219b70f713efd
Danny Brito <britodanny1997@gmail.com>
2021-03-17 16:44:11 -0400

    db - set for wsl2 and nodemon correct start

--- bin/app.js
+++ bin/app.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// const morgan = require('morgan')
+const morgan = require('morgan')
 
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 
-require('../util/database')
+const db = require('../util/database')
 
-// app.use(express.urlencoded({extended:false}))
+db.authenticate()
+    .then(() =>  {
+        console.log('Connection has been established successfully.')
+        return app.listen(process.env.PORT)
+    })
+    .then(() =>console.log(`server listen on: ${process.env.PORT}`))
+    .catch(error => console.error('Unable to connect to the database:',error))
+
+app.use(express.urlencoded({extended:false}))
--- util/database.js
+++ util/database.js
@@ -9,6 +9,5 @@ const sequelize = new Sequelize(
     dialect: 'postgres'
 })
 
-return sequelize.authenticate()
-            .then(() =>  console.log('Connection has been established successfully.'))
-            .catch(error => console.error('Unable to connect to the database:',error))
+
+module.exports = sequelize
