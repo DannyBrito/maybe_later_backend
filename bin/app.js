24bc51807881bffccccf4e61fc5dd88cb1e58ea6
Danny Brito <britodanny1997@gmail.com>
2021-03-17 14:17:50 -0400

    connecting db and env variables used

---
+++ bin/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+// const express = require('express');
+
+// const app = express();
+
+// const morgan = require('morgan')
+
+// app.use(morgan('dev'));
+
+require('../util/database')
+
+// app.use(express.urlencoded({extended:false}))
