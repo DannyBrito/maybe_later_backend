878fbdafae54bd3399ce10e4f78d8f410d730d5d
Danny Brito <britodanny1997@gmail.com>
2021-03-26 17:22:22 -0400

    modified paginator for better query handle

--- middleware/paginate.js
+++ middleware/paginate.js
@@ -1,24 +1,16 @@
 module.exports = model => { 
     return async (req, res, next) =>{
         const result = {}
-        let limit = parseInt(req.query.limit) || 5
-        limit = (limit < 5) ? 5 : limit
-        limit = Math.min(limit, 25)
-        let page = parseInt(req.query.page)
+        let limit = handleLimit(req.query.limit)
+        let page = handlePage(req.query.page)
         let offset = (page - 1) * limit
         try{
-            if( !offset || offset < 0){
-                result.records = []
-                result.totalRecords = await model.count()
-                result.totalPages = Math.ceil(result.totalRecords/limit)
-                result.next = {
-                    page: 1,
-                    limit
-                }
-                res.paginatedResults = result
-                return next()        
+            if(offset < 0){
+                limit = 0
+                offset = 0     
             }
             const{count:totalRecords,rows:records} = await model.findAndCountAll({limit,offset})
+            limit = limit || 5
             let totalPages = Math.ceil(totalRecords/limit)
             result.records = records
             result.totalRecords = totalRecords
@@ -26,22 +18,33 @@ module.exports = model => {
             result.currentPage = page
             if(page * limit < totalRecords){
                 result.next = {
-                    page: page + 1,
+                    page: (page < 0) ? 1 : page + 1,
                     limit
                 }
             }
-            // if page Upper Outer Range Return Last Page
             if(offset > 0){
                 result.previous = {
                     page: (totalPages < page) ? totalPages : page - 1,
                     limit
                 }
             }
-
             res.paginatedResults = result
             next()    
         }catch(error){
             next(error)
         }
     }
+}
+
+const handleLimit = (givenLimit = 5) =>{
+    givenLimit = parseInt(givenLimit)
+    if(!givenLimit || givenLimit < 5) givenLimit = 5
+    givenLimit = Math.min(25,givenLimit)
+    return givenLimit
+}
+
+const handlePage = (givenPage = 1) => {
+    givenPage = parseInt(givenPage) 
+    if(givenPage !== 0) givenPage = givenPage || 1
+    return givenPage
 }
